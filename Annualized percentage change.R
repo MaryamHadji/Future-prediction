d56a5c897c298e64b6b7c8c28c91d20a97822962
MaryamHadji <141636452+MaryamHadji@users.noreply.github.com>
2025-02-17 14:09:48 +0200

    Update Annualized percentage change.R

--- Annualized percentage change.R
+++ Annualized percentage change.R
@@ -1,4 +1,21 @@
-
+# R script for calculating annualized percentage change in hippocampal volume based on longitudinal MRI data
+# Author: Maryam Hadji, University of Eastern Finland, Kuopio, Finland (maryamh@uef.fi)
+# Last updated: 17-Feb-2025
+#
+# Requirements:
+# - R version 4.3.1 or later
+#
+# Usage:
+# - Set working directory to the directory containing this R script
+# - Run using: source('Annualized percentage change.R')
+#
+# Parameters:
+# - MRI_24: Data frame containing MRI measurements at timepoint 24 months
+# - MRI_48: Data frame containing MRI measurements at timepoint 48 months
+#
+# Returned values:
+# - A numeric vector representing the annualized percentage change in hippocampal volume
+#
 
 Change <- function(MRI_24, MRI_48) {
   hippo_24 <- rowMeans(MRI_24[, c("Right Hippocampus", "Left Hippocampus")], na.rm = TRUE)
